a9257b102e743e48a10f9da05a41efa334ee8015
Joaquin <joaqobianchi13@gmail.com>
2023-10-10 20:40:23 -0300

    estructura inicial de la Card para cada planta

--- client/app/tailwind.config.ts
+++ client/app/tailwind.config.ts
@@ -1,26 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        Poppins: ["Poppins", "sans-serif"],
-      },
-      colors: {
-        navbar: "#683B11",
-        "button-primary": "#104938",
-        "button-secondary": "#DCBE98",
-        background: "#E3F3F0",
-        text: "#61B78E",
-        "text-button": "",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				Poppins: ["Poppins", "sans-serif"],
+			},
+			colors: {
+				navbar: "#683B11",
+				"button-primary": "#104938",
+				"button-secondary": "#DCBE98",
+				background: "#E3F3F0",
+				text: "#61B78E",
+				"text-button": "",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
