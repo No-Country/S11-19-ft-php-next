90bc88cc08fb8f2b37e703bfb9aa22e27d2bdf1f
Joaquin <joaqobianchi13@gmail.com>
2023-10-12 02:54:02 -0300

    Seccion de beneficios y slider de recomendaciones

--- client/app/src/app/page.tsx
+++ client/app/src/app/page.tsx
@@ -2,30 +2,82 @@ import Image from "next/image";
 import Header from "@/components/header";
 import CarouselContainer from "@/components/carousel";
 export default function Home() {
-	
 	return (
 		<main className="min-h-screen ">
-			<Header/>
+			<Header />
 			<div className="mt-7">
-				<h2 className="text-lg text-center mb-4">Funciones</h2>
+				<h2 className="text-lg text-center font-bold mb-4">Funciones</h2>
 				<CarouselContainer>
 					<div className="bg-background flex flex-col justify-center items-center p-2 w-52 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-						<Image className="mt-3 w-20 h-20" src={''} alt="agregar-planta"/>
+						<Image className="mt-3 w-20 h-20" src={""} alt="agregar-planta" />
 						<h4 className="my-3">Agregar Planta</h4>
-						<p className="text-center text-sm ">Duis aute irure dolor in reprehenderit in voluptate velit</p>
+						<p className="text-center text-sm ">
+							Duis aute irure dolor in reprehenderit in voluptate velit
+						</p>
 					</div>
 					<div className="bg-background flex flex-col justify-center items-center p-2 w-52 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-						<Image className="mt-3 w-20 h-20" src={''} alt="Recordatorio"/>
+						<Image className="mt-3 w-20 h-20" src={""} alt="Recordatorio" />
 						<h4 className="my-3">Recordatorio</h4>
-						<p className="text-center text-sm ">Duis aute irure dolor in reprehenderit in voluptate velit</p>
+						<p className="text-center text-sm ">
+							Duis aute irure dolor in reprehenderit in voluptate velit
+						</p>
 					</div>
 					<div className="bg-background flex flex-col justify-center items-center p-2 w-52 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-						<Image className="mt-3 w-20 h-20" src={''} alt="Calendario"/>
+						<Image className="mt-3 w-20 h-20" src={""} alt="Calendario" />
 						<h4 className="my-3">Calendario</h4>
-						<p className="text-center text-sm ">Duis aute irure dolor in reprehenderit in voluptate velit</p>
+						<p className="text-center text-sm ">
+							Duis aute irure dolor in reprehenderit in voluptate velit
+						</p>
+					</div>
+				</CarouselContainer>
+				<h3 className="flex text-lg mt-7 mb-4 font-bold justify-center">
+					Beneficios
+				</h3>
+				<div className="flex flex-col ">
+					<Image src={""} className="w-full h-60 bg-slate-200 " alt="header" />
+					<ul className="flex flex-col items-center  mb-7 gap-5">
+						<li>Duis aute irure dolor in reprehenderit t</li>
+						<li>Duis aute irure dolor in reprehenderit i</li>
+						<li>Duis aute irure dolor in reprehenderit i</li>
+						<li>Duis aute irure dolor in reprehenderit i</li>
+						<li>Duis aute irure dolor in reprehenderit i</li>
+					</ul>
+				</div>
+				<h3 className="text-lg text-center font-bold mb-4">
+					Nuestros usuarios nos recomiendan
+				</h3>
+				<CarouselContainer>
+					<div className="bg-background flex flex-col justify-center items-center p-2 w-52 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+						<Image className="mt-3 w-20 h-20" src={""} alt="agregar-planta" />
+						<h4 className="my-3">Eduardo Gómez</h4>
+						<p className="text-center text-sm ">
+							Duis aute irure dolor in reprehenderit in voluptate velit
+						</p>
+					</div>
+					<div className="bg-background flex flex-col justify-center items-center p-2 w-52 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+						<Image className="mt-3 w-20 h-20" src={""} alt="Recordatorio" />
+						<h4 className="my-3">Susana Díaz</h4>
+						<p className="text-center text-sm ">
+							Duis aute irure dolor in reprehenderit in voluptate velit
+						</p>
+					</div>
+					<div className="bg-background flex flex-col justify-center items-center p-2 w-52 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+						<Image className="mt-3 w-20 h-20" src={""} alt="Calendario" />
+						<h4 className="my-3">Pablo Casas</h4>
+						<p className="text-center text-sm ">
+							Duis aute irure dolor in reprehenderit in voluptate velit
+						</p>
+					</div>
+					<div className="bg-background flex flex-col justify-center items-center p-2 w-52 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+						<Image className="mt-3 w-20 h-20" src={""} alt="Calendario" />
+						<h4 className="my-3">Rosario López</h4>
+						<p className="text-center text-sm ">
+							Duis aute irure dolor in reprehenderit in voluptate velit
+						</p>
 					</div>
 				</CarouselContainer>
 			</div>
 		</main>
 	);
 }
+// Falta el footer
--- client/app/src/components/header.tsx
+++ client/app/src/components/header.tsx
@@ -1,23 +1,32 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 const Header = () => {
-  return (
-    <header >
-      <nav className="flex justify-between items-center h-20 bg-background">
+	return (
+		<header>
+			<nav className="flex justify-between items-center h-20 bg-background">
+				<Image className="ml-2" src={""} alt="Brand" />
 
-        <Image className="ml-2" src={''} alt="Brand"/>
-        
-        <div className="text-sm mr-3">
-          <Link className="mr-7" href={'/'}>Registrarme</Link>
-          <Link className="p-2 bg-slate-700 text-slate-100 rounded-xl" href={'/'}>Iniciar Sesión</Link>
-        </div>
-      </nav>
-      <div className="flex flex-col ">
-        <Image src={''} className="w-full h-60 " alt="header"/>
-        <p className="mx-7 mt-6">Nombre es la app de cuidado de plantas que revolucionó la forma en que las personas cuidan y mantienen sus plantas</p>
-      </div>
-    </header>
-  )
-}
+				<div className="text-sm mr-3">
+					<Link className="mr-7" href={"/"}>
+						Registrarme
+					</Link>
+					<Link
+						className="p-2 bg-slate-700 text-slate-100 rounded-xl"
+						href={"/"}
+					>
+						Iniciar Sesión
+					</Link>
+				</div>
+			</nav>
+			<div className="flex flex-col ">
+				<Image src={""} className="w-full h-60 bg-slate-200 " alt="header" />
+				<p className="mx-7 mt-6">
+					Nombre es la app de cuidado de plantas que revolucionó la forma en que
+					las personas cuidan y mantienen sus plantas
+				</p>
+			</div>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
