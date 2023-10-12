f1f3a842a009cd8bce3014257a7ca21207c853f8
Gonzalo <garciacalvog@hotmail.com>
2023-10-12 16:40:20 -0300

    valibot parcialmente funcionando

--- client/app/src/app/testRoute/InputEmail.tsx
+++ client/app/src/app/testRoute/InputEmail.tsx
@@ -1,71 +1,90 @@
-'use client'
-import { useState } from "react"
+"use client";
+import { useState } from "react";
+import { email, minLength, object, type Output, parse, string } from 'valibot'
 
-function InputEmail({placeholder}:{placeholder:string}) {
 
-  const [inputValue, setInputValue] = useState('')
-  const [emailError, setEmailError] = useState(false)
-  const [inputBlured, setInputBlured] = useState(false)
-  const [emailValid, setEmailValid] = useState(false)
-  const PASS_REGEX = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{6,8}$/
-  const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-
-  const handleChange:React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    e.preventDefault()
-    setInputValue(e.target.value)
-    const testEmail = EMAIL_REGEX.test(e.target.value)
-    if (!testEmail && inputBlured) {
-      setEmailError(true)
-    }
-    if(testEmail) {
-      setEmailError(false)
-      setEmailValid(true)
-      console.log("emailValid")
-    }
-  }
-  const handleBlur:React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setInputBlured(true)
-    const testEmail = EMAIL_REGEX.test(e.target.value)
-    if (!testEmail) {
-      setEmailError(true)
-    }
-  }
-  console.log("inputBlured: ",inputBlured)
-  console.log(inputValue)
-  console.log("error: ", emailError)
-  return (
-    <>
-      <div>
-        <input
-          placeholder={placeholder}
-          value={inputValue}
-          onChange={handleChange}
-          required
-          className={
-            emailError?
-            "  border-2 border-[#FF0000] focus:border-[#FF0000] focus:border-[3px] focus:outline-none h-9 rounded-lg px-2"
-            :
-            " border-[#A7A7A7] border-2 focus:border-[#000000] focus:border-[3px] focus:outline-none h-9 rounded-lg px-2"
-          }
-          onBlur={handleBlur}
-        />
-        <span>
-        ✅
-        </span>
-      </div>
-      {emailError?
-        <p className="text-[#FF0000] visible" >
-        El email ingrasado no es válido
-        </p>
-        :
-        <p className="invisible">
-        El email ingrasado es válido
-        </p>
-        
-        
-      }
-      
-    </>
-  )
+interface inputemail {
+	placeholder: string;
 }
-export default InputEmail
+
+function InputEmail({ placeholder }: inputemail) {
+	const [inputValue, setInputValue] = useState("");
+	const [inputError, setInputError] = useState(false);
+	const [inputBlured, setInputBlured] = useState(false);
+	const [inputValid, setInputValid] = useState(false);
+
+	const LoginSchema = object({
+		email: string([email()]),
+		password: string([minLength(8)]),
+	});
+	
+	const PASS_REGEX = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{6,8}$/;
+	const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+
+	const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+		e.preventDefault();
+		setInputValue(e.target.value);
+		/* const testEmail = EMAIL_REGEX.test(e.target.value); */
+		try{
+			const testEmail = parse(LoginSchema, { email: inputValue, password: '45631lkjihi' });
+			if (!testEmail && inputBlured) {
+				setInputError(true);
+				setInputValid(false)
+			}
+			if (!testEmail) {
+				setInputValid(false)
+			}
+			if (testEmail) {
+				setInputError(false);
+				console.log("inputValid");
+				setInputValid(true)
+			}
+		}catch(err){
+			console.log("err en handleChange",err)
+		}
+	};
+	const handleBlur: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+		e.preventDefault()
+		setInputBlured(true);
+		/* const testEmail = EMAIL_REGEX.test(e.target.value); */
+		try {
+			const testEmail = parse(LoginSchema, { email: inputValue, password: '45631lkjihi' });
+			if (!testEmail) {
+				setInputError(true);
+			}
+		}
+		catch(err){
+			console.log("error en handleBlur:", err)
+		}
+	};
+	console.log("inputBlured: ", inputBlured);
+	console.log(inputValue);
+	console.log("error: ", inputError);
+	return (
+		<>
+			<div className="flex relative w-72">
+				<input
+					placeholder={placeholder}
+					value={inputValue}
+					onChange={handleChange}
+					className={
+						inputError
+							? "relative w-72  border-2 border-[#FF0000] focus:border-[#FF0000] focus:border-[3px] focus:outline-none h-9 rounded-lg px-2"
+							: "relative w-72 border-[#A7A7A7] border-2 focus:border-[#000000] focus:border-[3px] focus:outline-none h-9 rounded-lg px-2"
+					}
+					onBlur={handleBlur}
+				/>
+			{ inputValid? 	<span className="absolute left-[90%] top-[15%] visible">✅</span> : null }
+			{ inputError && inputBlured? <span className="absolute left-[90%] top-[15%] hidden">✅</span> : null }
+			</div>
+			{inputError ? (
+				<p className="text-[#FF0000] visible">
+					El email ingrasado no es válido
+				</p>
+			) : (
+				<p className="invisible">El email ingrasado es válido</p>
+			)}
+		</>
+	);
+}
+export default InputEmail;
--- client/app/src/app/testRoute/page.tsx
+++ client/app/src/app/testRoute/page.tsx
@@ -1,13 +1,17 @@
+"use client"
 import InputEmail from "./InputEmail";
-import './styles.testRoute.css'
+import "./styles.testRoute.css";
 export default function TestRoute() {
-  return(
-    <>
-      <form className="flex flex-col w-full">
-        <InputEmail placeholder="Email" />
-        <button >Enviar</button>
-      </form>
-    </>
 
-  )
+	const handleSubmit = (e:React.SyntheticEvent) => {
+		e.preventDefault();
+	}
+	return (
+		<>
+			<form onSubmit={handleSubmit} className="flex flex-col w-full">
+				<InputEmail placeholder="Email" />
+				<button className="w-48 border-2" type="submit">Enviar</button>
+			</form>
+		</>
+	);
 }
