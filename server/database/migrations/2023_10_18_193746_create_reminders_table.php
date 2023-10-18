3518bff653397245d7b5ecdf991778391ba8bc14
zightcode <122415240+zightcode@users.noreply.github.com>
2023-10-18 15:53:54 -0400

    Create model and migration reminder

---
+++ server/database/migrations/2023_10_18_193746_create_reminders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reminders', function (Blueprint $table) {
+            $table->id();
+            $table->string("frequency");
+            $table->string("days");
+            $table->date("warning_Time");
+            $table->enum("type",['reserved', 'delivered', 'cancelled'])->default('reserved');
+            $table->bool("repeat");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reminders');
+    }
+};
