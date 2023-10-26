701d6f1f2f8b18b94beccc3f8bdaa10d021a9f27
scarolayton <scarolayton@gmail.com>
2023-10-26 10:04:18 -0500

    feat: profile page

--- client/app/src/app/profile/page.tsx
+++ client/app/src/app/profile/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import Header from '@/components/header'
+import Image from 'next/image'
+import ProfilePicture from '@/assets/Ellipse-315.png'
+import Link from 'next/link'
 export default function Page() {
   return (
-    <div>
+    <section className='h-screen '>
       <Header/>
-    </div>
+      <h2 className='text-marron-oscuro font-semibold  p-6 pl-8 lg:pl-16 text-lg'>Mi Perfil</h2>
+      <div className='bg-[#BBEEE580] text-lg h-2/3 flex mx-9 lg:mx-32 rounded-xl flex-col justify-evenly items-center'>
+        <Image src={ProfilePicture} className='w-52' alt='avatar' />
+        <p>Sandra Pacheco</p>
+        <p>Sandra@gmail.com</p>
+        <Link className='text-base p-3 px-6 rounded-md bg-primary text-slate-100' href={'/profile/edit-profile'}>
+          Editar el perfil
+        </Link>
+      </div>
+    </section>
   )
 }
