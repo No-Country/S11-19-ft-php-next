a93b7be3bf3e00de1d1f8837d1ef7099cc82f1b9
Ignacio <47858291+byZhetta@users.noreply.github.com>
2023-10-31 15:22:55 -0300

    refactor: profile controller

--- server/app/Http/Controllers/ProfileController.php
+++ server/app/Http/Controllers/ProfileController.php
@@ -6,10 +6,7 @@ use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules\Password;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
@@ -59,60 +56,5 @@ class ProfileController extends Controller
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
-    }
-
-    public function showProfile()
-    {
-        $user = DB::table('users')
-        ->select(DB::raw("CONCAT(name,' ',lastname) AS name"), 'email', 'img')
-        ->where('id', auth()->id())
-        ->get();
-        
-        return response([
-            "message" => 'Profile user',
-            "data" => $user
-        ],200); 
-    }
-
-    public function updateProfile(Request $request)
-    {
-        // Update name, lastname, email and image
-        try {
-            DB::table('users')
-            ->where('id', '=', auth()->id())
-            ->update($request->input());
-
-            $user = DB::table('users')
-            ->select(DB::raw("CONCAT(name,' ',lastname) AS name"), 'email', 'img')
-            ->where('id', auth()->id())
-            ->get();
-            
-            return response([
-                "message" => 'Updated profile successfully',
-                "data" => $user
-            ],200); 
-        } catch (\Throwable $e) {
-            return response(["error" => $e->getMessage()],500); 
-        }
-    }
-
-    public function updatePassword(Request $request)
-    {
-        try {
-            $validated = $request->validateWithBag('updatePassword', [
-                'current_password' => ['required', 'current_password'],
-                'password' => ['required', Password::defaults(), 'confirmed'],
-            ]);
-
-            DB::table('users')
-            ->where('id', '=', auth()->id())
-            ->update([
-                'password' => Hash::make($validated['password']),
-            ]);
-
-            return response(["message" => 'Updated password successfully'],200); 
-        } catch (\Throwable $e) {
-            return response(["error" => $e->getMessage()],500); 
-        }
     }
 }
