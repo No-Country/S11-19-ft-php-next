7d99993f01c18ccf83a3a35ffe7081ce5461eb85
Ignacio <47858291+byZhetta@users.noreply.github.com>
2023-10-31 15:36:34 -0300

    refactor: organizing admin controllers and routes

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\NotificationController;
 use Illuminate\Support\Facades\Route;
 
