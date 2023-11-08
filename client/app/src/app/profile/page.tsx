089793c9d2d73742b81db04591240b80a81426f7
Gonzalo <garciacalvog@hotmail.com>
2023-11-08 02:07:21 -0300

    logout

--- client/app/src/app/profile/page.tsx
+++ client/app/src/app/profile/page.tsx
@@ -4,7 +4,6 @@ import Header from '@/components/header'
 import { AuthContext } from '@/components/authcontext';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 /* type User = {
 	name:string,
@@ -16,7 +15,6 @@ import { useRouter } from 'next/navigation';
 export default function Page() {
 
 	const { userState, logOutUser } = useContext(AuthContext);
-	const router = useRouter()
   /* const [user, setUser] = useState<User | null>(null)*/
 
 
@@ -28,18 +26,16 @@ export default function Page() {
       <div className='bg-[#BBEEE580] text-lg h-2/3 flex mx-9 lg:mx-32 rounded-xl flex-col justify-evenly items-center'>
           {userState?.img ? (
           <Image src={userState?.img} width={100} height={100} alt="avatar"/>					
-          ): 
-					(
-						<p className="bg-background rounded-full w-36 h-36 p-2 pl-3 pt-10 text-7xl text-center font-semibold text-marron-oscuro">
-							{userState?.name?.slice(0,1)}
-						</p>
+          ): (
+          <p className="bg-background rounded-full w-36 h-36 p-2 pl-3 pt-10 text-7xl text-center font-semibold text-marron-oscuro">
+						{userState?.name?.slice(0,1)}</p>
           )}
         <p>{userState?.name}</p>
         <p>{userState?.email}</p>
         <Link className='text-base p-3 px-6 rounded-md bg-primary text-slate-100' href={'/profile/edit-profile'}>
           Editar el perfil
         </Link>
-				<button className='text-base p-3 px-6 rounded-md bg-primary text-slate-100' onClick={() => logOutUser(router.push("/"))}>Cerrar sesión</button>
+				<button onClick={logOutUser}>Cerrar sesión</button>
       </div>
     </section>
   )
